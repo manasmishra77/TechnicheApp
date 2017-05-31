a1d9b536bad0f0f6a827219c0d567566758474af
Manas Mishra <manasmishra@Manas-MacBook-Air.local>
2017-05-31 16:31:34 +0530

    Initial Commit

---
+++ TechNiche/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechNiche
+//
+//  Created by Manas Mishra on 31/05/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
